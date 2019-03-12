3021ff9c52be96f994021504e451ede9c6ef6724
DavidFoden <davidfoden.martinez@gmail.com>
2019-03-12 16:32:56 +0100

    This is Rocket Booster Data
    Commiting a lot

---
+++ Rocket_Booster/Assets/Oscillator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float peroid = 2f;
+
+    [Range(0,1)] [SerializeField] float movementFactor;
+
+    Vector3 startingPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (peroid <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / peroid; // grows continuelly from 0
+
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
---
+++ Rocket_Booster/Assets/Rocket.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float MainThrust = 100f;
+    [SerializeField] float levelLoadDelay = 2f;
+
+
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip success;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+
+    AudioSource RocketBoost;
+    Rigidbody rigidBody;
+
+    enum State { Alive, Dying, Transcending }
+ State state = State.Alive;
+
+ bool collisionsAreEnabled = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RocketBoost = GetComponent<AudioSource>();
+        rigidBody = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        if (state == State.Alive)
+        {
+        RespondToThrustInput();
+        RespondToRotateInput();
+       }
+       if (Debug.isDebugBuild) {
+       RespondToDebugKeys();
+      }
+    }
+private void RespondToDebugKeys()
+ {
+    if(Input.GetKeyDown(KeyCode.L)) {
+        LoadNextLevel();
+    }
+    else if(Input.GetKeyDown(KeyCode.C)) {
+        collisionsAreEnabled = !collisionsAreEnabled;
+    }
+}
+
+    void OnCollisionEnter(Collision collision) {
+        if (state != State.Alive || !collisionsAreEnabled ) {
+            return;
+        }
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+        // do nothing
+            break;
+            case "Finish":
+           StartSuccessSequence();
+            break;
+        
+           default:
+           StartDeathSequene();
+           break;
+        }
+    }
+
+    private void StartDeathSequene() {
+          state = State.Dying;
+          RocketBoost.Stop();
+        RocketBoost.PlayOneShot(death);
+        deathParticles.Play();          
+        Invoke("LoadFirstLevel", levelLoadDelay); 
+    }
+
+    private void StartSuccessSequence() {
+         state = State.Transcending;
+             RocketBoost.Stop();
+           RocketBoost.PlayOneShot(success);
+           successParticles.Play();
+           Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void LoadNextLevel() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    
+
+    private void LoadFirstLevel() {
+        SceneManager.LoadScene(0);
+    }
+    
+
+     private void RespondToThrustInput() { 
+        if (Input.GetKey(KeyCode.Space)) {
+            ApplyThrust();
+        }
+            else {
+                RocketBoost.Stop();
+                mainEngineParticles.Stop();
+
+            }
+        }
+        private void ApplyThrust() {
+            rigidBody.AddRelativeForce(Vector3.up * MainThrust * Time.deltaTime);
+            if (!RocketBoost.isPlaying) { // So it does not layer the sound
+            RocketBoost.PlayOneShot(mainEngine);
+            }
+            mainEngineParticles.Play();
+        }
+        
+    private void RespondToRotateInput() { //Roatate My ass
+         rigidBody.freezeRotation = true; // Take manual control of rotation
+        
+         float rotationSpeed = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A)) {
+          transform.Rotate(Vector3.forward * rotationSpeed); // here as well down!
+        }
+        
+       else if (Input.GetKey(KeyCode.D)) {
+          transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+         rigidBody.freezeRotation = false; // resume physices control of rotation
+    }
+}
